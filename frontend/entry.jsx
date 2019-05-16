74bd289119938572601b6435168c386b884d30a0
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-15 17:58:27 -0700

    Remove unecessary dependencies and files

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>HAVE NO FEAR REACT IS HERE</h1>, root);
-})
+  root.append('HAVE NO FEAR REACT IS HERE');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,6 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: './bundle.js'
   },
-  module: {
-    rules: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        }
-      }
-    ]
-  },
   devtool: 'inline-source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
